b82b74eb9a84f0ca8f78038ab8eacd203511be45
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-03-30 02:22:37 +0300

    Position struct created

--- contracts/FarmlyPositionManager.sol
+++ contracts/FarmlyPositionManager.sol
@@ -1,4 +1,10 @@
 pragma solidity >=0.5.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract FarmlyPositionManager {}
+contract FarmlyPositionManager {
+    struct Position {
+        address executer;
+        address owner;
+        uint256 debtShare;
+    }
+}
