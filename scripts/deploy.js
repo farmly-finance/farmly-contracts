e7849967948186ebc20094543af81da5e2c7eda9
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-03-30 07:18:23 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,44 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const ROUTER = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"
+  const VAULT_TOKEN = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
+  const USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
+  /*
+    const FarmlyConfig = await hre.ethers.getContractFactory("FarmlyConfig");
+    const farmlyConfig = await FarmlyConfig.deploy();
+    await farmlyConfig.deployed();
+    console.log(
+      `FarmlyConfig deployed to ${farmlyConfig.address}`
+    );
+  
+    const FarmlyVault = await hre.ethers.getContractFactory("FarmlyVault");
+    const farmlyVault = await FarmlyVault.deploy(VAULT_TOKEN);
+    await farmlyVault.deployed();
+    console.log(
+      `FarmlyVault deployed to ${farmlyVault.address}`
+    );
+  
+    console.log((await farmlyConfig.setFarmingPoolVault(USDC, VAULT_TOKEN, farmlyVault.address)).hash, "setFarmingPoolVault");
+  */
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  const FarmlyPositionManager = await hre.ethers.getContractFactory("FarmlyPositionManager");
+  const farmlyPositionManager = await FarmlyPositionManager.deploy("0xD5Ccf53b66af850D237da0c695Bfcbd9b77f2fe4");
+  await farmlyPositionManager.deployed();
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `FarmlyPositionManager deployed to ${farmlyPositionManager.address}`
   );
+
+  /*
+    const FarmlyDexExecutor = await hre.ethers.getContractFactory("FarmlyDexExecutor");
+    const farmlyDexExecutor = await FarmlyDexExecutor.deploy(ROUTER);
+    await farmlyDexExecutor.deployed();
+    console.log(
+      `FarmlyDexExecutor deployed to ${farmlyDexExecutor.address}`
+    );
+    /*
+      console.log((await farmlyConfig.setExecutor(farmlyDexExecutor.address, "true")).hash, "setExecutor");
+    */
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -30,3 +53,9 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+/* 
+
+unlockTime, { value: lockedAmount }
+ 
+
+  */
