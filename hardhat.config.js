418d4277a0cacea3beb1ecd2d80c64bd573f0905
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-03-30 18:30:27 +0300

    bsc testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,13 @@ module.exports = {
     arbitrumOne: {
       url: "https://arb1.arbitrum.io/rpc",
       accounts: [process.env.DEPLOYER_KEY]
-    }
+    },
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      gas: 3000000,
+      accounts: [process.env.DEPLOYER_KEY]
+    },
   }
 };
