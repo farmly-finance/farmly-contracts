a9b56d2ca771e02529fb01661bc521b1c78d7eec
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-03-30 05:25:46 +0300

    arbitrumOne network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.15",
+  networks: {
+    arbitrumOne: {
+      url: "https://arb1.arbitrum.io/rpc",
+      //accounts: [ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY]
+    }
+  }
 };
