abca877ea8567df5cf5ebcbc63e42773e090bad9
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-03-30 04:11:54 +0300

    Create IFarmlyDexExecutor.sol

---
+++ contracts/interfaces/IFarmlyDexExecutor.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.5.0;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IFarmlyDexExecutor {
+    function execute(
+        IERC20 token0,
+        IERC20 token1,
+        uint256 amount0,
+        uint256 amount1
+    ) external;
+}
