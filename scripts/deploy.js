8f948f9dcc9a2c5475e0dc0e7cd024fec6ce1441
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-03-30 18:30:45 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,9 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+require('dotenv').config();
 
 async function main() {
-  const ROUTER = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"
-  const VAULT_TOKEN = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
-  const USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
   /*
     const FarmlyConfig = await hre.ethers.getContractFactory("FarmlyConfig");
     const farmlyConfig = await FarmlyConfig.deploy();
@@ -19,33 +17,33 @@ async function main() {
     );
   
     const FarmlyVault = await hre.ethers.getContractFactory("FarmlyVault");
-    const farmlyVault = await FarmlyVault.deploy(VAULT_TOKEN);
+    const farmlyVault = await FarmlyVault.deploy(process.env.VAULT_TOKEN);
     await farmlyVault.deployed();
     console.log(
       `FarmlyVault deployed to ${farmlyVault.address}`
     );
   
-    console.log((await farmlyConfig.setFarmingPoolVault(USDC, VAULT_TOKEN, farmlyVault.address)).hash, "setFarmingPoolVault");
+    console.log((await farmlyConfig.setFarmingPoolVault(process.env.BASE_TOKEN, process.env.VAULT_TOKEN, farmlyVault.address)).hash, "setFarmingPoolVault");
+  
+  
+  
+    const FarmlyPositionManager = await hre.ethers.getContractFactory("FarmlyPositionManager");
+    const farmlyPositionManager = await FarmlyPositionManager.deploy(farmlyConfig.address);
+    await farmlyPositionManager.deployed();
+    console.log(
+      `FarmlyPositionManager deployed to ${farmlyPositionManager.address}`
+    );
   */
-  /*
-
-  const FarmlyPositionManager = await hre.ethers.getContractFactory("FarmlyPositionManager");
-  const farmlyPositionManager = await FarmlyPositionManager.deploy("0xD5Ccf53b66af850D237da0c695Bfcbd9b77f2fe4");
-  await farmlyPositionManager.deployed();
-  console.log(
-    `FarmlyPositionManager deployed to ${farmlyPositionManager.address}`
-  );
-    */
 
   const FarmlyDexExecutor = await hre.ethers.getContractFactory("FarmlyDexExecutor");
-  const farmlyDexExecutor = await FarmlyDexExecutor.deploy(ROUTER);
+  const farmlyDexExecutor = await FarmlyDexExecutor.deploy(process.env.PANCAKE_ROUTER);
   await farmlyDexExecutor.deployed();
   console.log(
     `FarmlyDexExecutor deployed to ${farmlyDexExecutor.address}`
   );
-  /*
-    console.log((await farmlyConfig.setExecutor(farmlyDexExecutor.address, "true")).hash, "setExecutor");
-  */
+
+  // console.log((await farmlyConfig.setExecutor(farmlyDexExecutor.address, "true")).hash, "setExecutor");
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -54,9 +52,3 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-/* 
-
-unlockTime, { value: lockedAmount }
- 
-
-  */
