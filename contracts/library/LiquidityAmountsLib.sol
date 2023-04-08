7e487e68d803946b87526921ff89f6245a975cd8
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-04-08 19:06:15 +0300

    Create LiquidityAmountsLib.sol

---
+++ contracts/library/LiquidityAmountsLib.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.0;
+import "@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol";
+
+contract LiquidityAmountsLib {
+    function getLiquidityForAmounts(
+        uint160 sqrtRatioX96,
+        uint160 sqrtRatioAX96,
+        uint160 sqrtRatioBX96,
+        uint256 amount0,
+        uint256 amount1
+    ) public pure returns (uint128 liquidity) {
+        return
+            LiquidityAmounts.getLiquidityForAmounts(
+                sqrtRatioX96,
+                sqrtRatioAX96,
+                sqrtRatioBX96,
+                amount0,
+                amount1
+            );
+    }
+
+    function getAmountsForLiquidity(
+        uint160 sqrtRatioX96,
+        uint160 sqrtRatioAX96,
+        uint160 sqrtRatioBX96,
+        uint128 liquidity
+    ) public pure returns (uint256 amount0, uint256 amount1) {
+        (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
+            sqrtRatioX96,
+            sqrtRatioAX96,
+            sqrtRatioBX96,
+            liquidity
+        );
+    }
+}
