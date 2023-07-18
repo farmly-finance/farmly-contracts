45f388716a2ac2afe8f5abe4143ca58cd92b7580
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-07-18 13:38:17 +0300

    Create positionUSDValue.js

---
+++ scripts/view/positionUSDValue.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+require('dotenv').config();
+
+async function main() {
+
+    const FarmlyPositionManager = await ethers.getContractFactory("FarmlyPositionManager");
+    const farmlyPositionManager = await FarmlyPositionManager.attach(
+        process.env.FARMLY_POSITION_MANAGER_CONTRACT_ADDRESS
+    );
+
+
+    const usdValue = await farmlyPositionManager.getPositionUSDValue(process.env.FARMLY_UNIV3_EXECUTOR_CONTRACT_ADDRESS, "1")
+    console.log(usdValue.token0USD / 1e18, "token0USD")
+    console.log(usdValue.token1USD / 1e18, "token1USD")
+    console.log(usdValue.totalUSD / 1e18, "totalUSD");
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
