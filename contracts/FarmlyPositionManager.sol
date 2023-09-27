0d3e51a5060fc7f97a912cc0adbb4268ffba2d74
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-09-27 12:17:56 +0300

    IFarmlyPositionManager

--- contracts/FarmlyPositionManager.sol
+++ contracts/FarmlyPositionManager.sol
@@ -3,87 +3,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./library/FarmlyFullMath.sol";
 import "./library/FarmlyStructs.sol";
 import "./library/FarmlyTransferHelper.sol";
-import "./interfaces/IFarmlyVault.sol";
-import "./interfaces/IFarmlyUniV3Executor.sol";
 import "./interfaces/IFarmlyPriceConsumer.sol";
 import "./interfaces/IFarmlyConfig.sol";
 import "./interfaces/IFarmlyUniV3Reader.sol";
+import "./interfaces/IFarmlyPositionManager.sol";
 
-contract FarmlyPositionManager {
-    struct SlippageProtection {
-        uint256 minPositionUSDValue;
-        uint256 maxLeverageTolerance; // 1000000 = 1x
-    }
-
-    struct VaultInfo {
-        IFarmlyVault vault;
-        uint debtAmount;
-    }
-
-    struct DebtInfo {
-        VaultInfo vault;
-        uint debtShare;
-    }
-
-    struct Position {
-        uint uniV3PositionID;
-        address owner;
-        DebtInfo debt0;
-        DebtInfo debt1;
-    }
-
-    struct CreatePositionParams {
-        IFarmlyUniV3Executor executor;
-        uint amount0;
-        uint amount1;
-        VaultInfo vault0;
-        VaultInfo vault1;
-        FarmlyStructs.PositionInfo positionInfo;
-        FarmlyStructs.SwapInfo swapInfo;
-        SlippageProtection slippage;
-    }
-
-    struct IncreasePositionParams {
-        uint256 positionID;
-        IFarmlyUniV3Executor executor;
-        uint amount0;
-        uint amount1;
-        uint debtAmount0;
-        uint debtAmount1;
-        FarmlyStructs.SwapInfo swapInfo;
-        SlippageProtection slippage;
-    }
-
-    struct DecreasePositionParams {
-        IFarmlyUniV3Executor executor;
-        uint positionID;
-        uint24 decreasingPercent;
-    }
-
-    struct CollectFeesParams {
-        IFarmlyUniV3Executor executor;
-        uint256 positionID;
-    }
-
-    struct CollectAndIncreaseParams {
-        IFarmlyUniV3Executor executor;
-        uint256 positionID;
-        uint256 debt0;
-        uint256 debt1;
-        FarmlyStructs.SwapInfo swapInfo;
-        SlippageProtection slippage;
-    }
-
-    struct ClosePositionParams {
-        IFarmlyUniV3Executor executor;
-        uint256 positionID;
-    }
-
-    struct LiquidatePositionParams {
-        IFarmlyUniV3Executor executor;
-        uint256 positionID;
-    }
-
+contract FarmlyPositionManager is IFarmlyPositionManager {
     uint256 constant MAX_INT =
         0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
---
+++ contracts/interfaces/IFarmlyPositionManager.sol
@@ -0,0 +1,80 @@
+pragma solidity >=0.5.0;
+import "./IFarmlyUniV3Executor.sol";
+import "./IFarmlyVault.sol";
+import "../library/FarmlyStructs.sol";
+
+interface IFarmlyPositionManager {
+    struct SlippageProtection {
+        uint256 minPositionUSDValue;
+        uint256 maxLeverageTolerance; // 1000000 = 1x
+    }
+
+    struct VaultInfo {
+        IFarmlyVault vault;
+        uint debtAmount;
+    }
+
+    struct DebtInfo {
+        VaultInfo vault;
+        uint debtShare;
+    }
+
+    struct Position {
+        uint uniV3PositionID;
+        address owner;
+        DebtInfo debt0;
+        DebtInfo debt1;
+    }
+
+    struct CreatePositionParams {
+        IFarmlyUniV3Executor executor;
+        uint amount0;
+        uint amount1;
+        VaultInfo vault0;
+        VaultInfo vault1;
+        FarmlyStructs.PositionInfo positionInfo;
+        FarmlyStructs.SwapInfo swapInfo;
+        SlippageProtection slippage;
+    }
+
+    struct IncreasePositionParams {
+        uint256 positionID;
+        IFarmlyUniV3Executor executor;
+        uint amount0;
+        uint amount1;
+        uint debtAmount0;
+        uint debtAmount1;
+        FarmlyStructs.SwapInfo swapInfo;
+        SlippageProtection slippage;
+    }
+
+    struct DecreasePositionParams {
+        IFarmlyUniV3Executor executor;
+        uint positionID;
+        uint24 decreasingPercent;
+    }
+
+    struct CollectFeesParams {
+        IFarmlyUniV3Executor executor;
+        uint256 positionID;
+    }
+
+    struct CollectAndIncreaseParams {
+        IFarmlyUniV3Executor executor;
+        uint256 positionID;
+        uint256 debt0;
+        uint256 debt1;
+        FarmlyStructs.SwapInfo swapInfo;
+        SlippageProtection slippage;
+    }
+
+    struct ClosePositionParams {
+        IFarmlyUniV3Executor executor;
+        uint256 positionID;
+    }
+
+    struct LiquidatePositionParams {
+        IFarmlyUniV3Executor executor;
+        uint256 positionID;
+    }
+}
