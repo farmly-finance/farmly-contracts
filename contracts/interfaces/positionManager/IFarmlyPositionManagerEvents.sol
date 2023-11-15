42db2dbdf249593cb071f39e908d26ac5ad83b17
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-11-15 15:50:00 +0300

    Some event elements deleted

--- contracts/interfaces/positionManager/IFarmlyPositionManagerEvents.sol
+++ contracts/interfaces/positionManager/IFarmlyPositionManagerEvents.sol
@@ -82,23 +82,15 @@ interface IFarmlyPositionManagerEvents {
     /// @notice Emitted when the existing position rewards collected and increased
     /// @param uniV3PositionID Token id of Uniswap V3 position
     /// @param positionID Position id of position
-    /// @param token0 The token0 contract address of position
-    /// @param token1 The token1 contract address of position
     /// @param collectedAmount0 The collected amount of token0
     /// @param collectedAmount1 The collected amount of token1
-    /// @param amount0 The increased amount of token0
-    /// @param amount1 The increased amount of token1
     /// @param debtAmount0 The new debt amount of token0
     /// @param debtAmount1 The new debt amount of token1
     event FeesCollectedAndPositionIncreased(
         uint256 indexed uniV3PositionID,
         uint256 positionID,
-        address indexed token0,
-        address indexed token1,
         uint256 collectedAmount0,
         uint256 collectedAmount1,
-        uint256 amount0,
-        uint256 amount1,
         uint256 debtAmount0,
         uint256 debtAmount1
     );
